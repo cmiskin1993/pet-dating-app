019e6914f882007d377db0de37b93d5d7b9bc289
Carrie Miskin <clove40@gmail.com>
2022-05-16 15:10:45 -0400

    added navigation

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
 import React from 'react'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+
+import Home from './components/Home'
+import NavBar from './components/NavBar'
+import Feed from './components/profile/Feed'
+import UserProfile from './components/profile/UserProfile'
+import Login from './components/session/Login'
+import Signup from './components/session/Signup'
 
 const App = () => {
   return (
-    <div className="App">
-      <h1>Hello Wrold!</h1>
-    </div>
+    <Router>
+    <NavBar />
+      <Routes>
+        <Route path="/" element ={<Home />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/profile" element={<UserProfile/>} />
+        <Route path="/feed" element={<Feed />} />
+      </Routes>
+    </Router>
   )
 }
 
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,8 +1,17 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const NavBar = () => {
   return (
-    <div>NavBar</div>
+    <div>
+        <ul>
+            <li><Link to="/">Home</Link></li>
+            <li><Link to="/signup">Create Account</Link></li>
+            <li><Link to="/login">Login</Link></li>
+            <li><Link to="/profile">My Profile</Link></li>
+            <li><Link to="/feed">Find My BestFriend</Link></li>
+        </ul>
+    </div>
   )
 }
 
